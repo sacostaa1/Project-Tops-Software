101222fc03a8b22a9099ad812fb602d2b8d9caa5
Jeronimo Perez Baquero <perezjeronimo35@gmail.com>
2025-03-15 17:43:09 -0500

    Started project CarsDealer

---
+++ CarsDealer/src/main/java/com/dealer/carsdealer/CarsDealerApplication.java
@@ -0,0 +1,13 @@
+package com.dealer.carsdealer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsDealerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsDealerApplication.class, args);
+	}
+
+}
---
+++ CarsDealer/src/test/java/com/dealer/carsdealer/CarsDealerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dealer.carsdealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsDealerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
