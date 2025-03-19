ad0c02aae84d2ca36ff11f70bddb89ec513a1bc7
Samuel Acosta <sacostaa1@eafit.edu.co>
2025-03-18 22:31:32 -0500

    front catalogo

---
+++ CarsDealer/src/main/java/com/dealer/carsdealer/controllers/CarController.java
@@ -0,0 +1,79 @@
+package com.dealer.carsdealer.controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.dealer.carsdealer.models.Car;
+import com.dealer.carsdealer.repositories.CarRepository;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class CarController {
+
+    @Autowired
+    private CarRepository carRepository;
+
+    
+    
+    @GetMapping("/cars/catalogo")
+    public String showCatalogoPage(Model model) {
+        List<Car> cars = carRepository.findAll();  
+        model.addAttribute("cars", cars);
+        return "cars/catalogo";  
+    }
+
+    @GetMapping("/car-add")
+    public String addCarForm(Model model) {
+        model.addAttribute("car", new Car());
+        return "car-add";
+    }
+
+    @GetMapping("/car-list")
+    public String showCarList(Model model) {
+        List<Car> cars = carRepository.findAll();
+        model.addAttribute("cars", cars);
+        return "car-list";
+    }
+
+    @GetMapping("/car-detail/{id}")
+    public String showCarDetail(@PathVariable int id, Model model) {
+        Optional<Car> car = carRepository.findById(id);
+        if (car.isPresent()) {
+            model.addAttribute("car", car.get());
+            return "car-detail";
+        } else {
+            return "redirect:/car-list";
+        }
+    }
+
+    @GetMapping("/car-delete/{id}")
+    public String deleteCar(@PathVariable int id) {
+        carRepository.deleteById(id);
+        return "redirect:/car-list";
+    }
+
+
+    @PostMapping("/car/save")
+    public String saveCar(@Valid @ModelAttribute("car") Car car, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("car", car);
+            return "car-add";
+        }
+
+        carRepository.save(car);
+
+        model.addAttribute("message", "Elemento creado satisfactoriamente");
+        return "car-success";
+    }
+
+}
